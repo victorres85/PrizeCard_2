001b6b1141aafb250a44f012ce20b02780323378
victorres85 <victorres.almeida@gmail.com>
2023-01-18 11:09:40 +0000

    Adding authentication

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.9-alpine3.13
 LABEL mantainer="victoralmeida"
 
-ENV PYTHONUNBUFFERED 1
+ENV PYTHONBUFFERED 1
 
 COPY ./requirements.txt /tmp/requirements.txt
 COPY ./requirements.dev.txt /tmp/requirements.dev.txt
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -110,6 +110,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
 class PrivateUserApiTests(TestCase):
     ''' Test API requests that require authentication.'''
 
